389d493e834f5cbd942b702d6ba31aaf698e30bf
macbook <macbook@MacBook-Pro-macbook.local>
2024-08-14 06:20:39 +0500

    some feature add

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ bs = BeautifulSoup(dr.page_source,"html.parser")
 res = bs.find_all('a', class_="subtitle-item")
 # title = bs.select_one('div.subtitle')
 # print(bs.prettify())
-button_title = bs.find('button', class_="ui-component ui-button add-to-cart tertiary-outlined small fill")['title']
+button_prices = [price.text.strip() for price in bs.find_all('span', class_="currency product-card-price slightly medium")]
+button_feedback = [feedback.text.strip() for feedback in bs.find_all('span', class_="orders__feedback-amount")]
+button_titles = [button['title'] for button in bs.find_all('button', class_="ui-component ui-button add-to-cart tertiary-outlined small fill")]
 
-print(button_title)
+for title, price, feedback in zip(button_titles, button_prices, button_feedback):
+    print(f"Nom: {title}\nNarxi: {price}\nFeedbacks: {feedback}\n")
